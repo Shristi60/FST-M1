58bb3671b82941e88998fab11f42233db27f47bc
Shristi60 <92945971+Shristi60@users.noreply.github.com>
2025-08-07 02:10:10 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity3.java
@@ -0,0 +1,29 @@
+package activities;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity3 {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		//Open the training support site. (https://training-support.net)
+		driver.get("https://training-support.net/");
+
+        //Print the title of the page
+		System.out.println("The title of page is: "+ driver.getTitle());
+
+        //Click the about us button
+		Thread.sleep(Duration.ofSeconds(3));
+		driver.findElement(By.xpath("//a[text()='About Us']")).click();
+
+        //Print the title of the new page
+		Thread.sleep(2000);
+		System.out.println("The title of page is: "+ driver.getTitle());
+		driver.quit();
+	}
+
+}
