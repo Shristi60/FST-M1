7bc72e9c1f781039cfdee9057dfa78147f8af57e
Shristi60 <92945971+Shristi60@users.noreply.github.com>
2025-08-20 00:36:52 +0530

    Add files via upload

---
+++ Java/Activities/Book.java
@@ -0,0 +1,13 @@
+package activities;
+
+public abstract class Book {
+	
+	String title;
+	 public abstract void setTitle(String name);
+	 
+	 public String getTitle()
+	 {
+		 return title;
+	 }
+
+}
